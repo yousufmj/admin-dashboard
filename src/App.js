cacfa4266352242843a330d6e04128de70bae437
yousufmj <yousufmjaleel@gmail.com>
2018-05-17 16:06:04 +0100

    setting up basic dashboard view

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { PostList } from './posts';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} />
+    </Admin>
+);
+
+export default App;
