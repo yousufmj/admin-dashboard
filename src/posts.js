be3adc101141a6df4a2de0abba51a06fd0f3f10b
yousufmj <yousufmjaleel@gmail.com>
2018-05-17 17:17:32 +0100

    updating posts

--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Datagrid, TextField, EmailField, ReferenceField } from 'react-admin';
+import { List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput } from 'react-admin';
 
 export const PostList = (props) => (
     <List {...props}>
@@ -10,6 +10,36 @@ export const PostList = (props) => (
             </ReferenceField>
             <TextField source="title" />
             <TextField source="body" />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = (props) => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
 );
