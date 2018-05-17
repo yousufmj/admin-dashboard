b1d1ccdbb7ad077032db43c252c2a5b8b2bd8da1
yousufmj <yousufmjaleel@gmail.com>
2018-05-17 16:22:11 +0100

    viewing dummy api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { PostList } from './posts';
+import { UserList } from './users';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
     </Admin>
 );
 
--- src/posts.js
+++ src/posts.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField, ReferenceField } from 'react-admin';
 
 export const PostList = (props) => (
     <List {...props}>
         <Datagrid>
             <TextField source="id" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
             <TextField source="title" />
             <TextField source="body" />
         </Datagrid>
