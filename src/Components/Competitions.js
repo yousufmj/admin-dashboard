dcbadd1c7ed48b43222f4aa81dfccdff7c64321f
yousufmj <yousufmjaleel@gmail.com>
2018-05-18 14:10:23 +0100

    creating cutsom dataProvider and competition component

---
+++ src/Components/Competitions.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput,
+    LongTextInput, ReferenceInput, SelectInput, SimpleForm, TextInput, Filter,SimpleList } from 'react-admin';
+
+export const CompetitionList = (props) => (
+    <List {...props}  filters={<CompetitionFilter />}>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <TextField source="body" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+const CompetitionTitle = ({ record }) => {
+    return <span>Competition {record ? `"${record.title}"` : ''}</span>;
+};
+
+const CompetitionFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+export const CompetitionEdit = (props) => (
+    <Edit title={<CompetitionTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const CompetitionCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
+);
